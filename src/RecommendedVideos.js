d8bd86fccd75bb3498742d97bf244c6937f26a9c
DAMI KOMOLAFE <damik94@gmail.com>
2020-09-03 22:02:28 -0400

    first commit

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./RecommendedVideos.css"
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            <h1>Videos</h1>
+        </div>
+    )
+}
+
+export default RecommendedVideos
